83a9b6e9f9caf41a6b935c9f07264afd1e573dd3
sohanxsahoo <sohanxsahoo@gmail.com>
2025-03-07 20:06:28 +0530

    func recursion done

--- 02 Do Coding Exercise/Practice Set 05/problem_5.c
+++ 02 Do Coding Exercise/Practice Set 05/problem_5.c
@@ -3,3 +3,13 @@
 int a = 4;
 printf("%d %d %d \n", a, ++a, a++);
 */
+
+#include <stdio.h>
+
+int main(){
+    int a = 4; 
+    printf("%d %d %d \n", a, ++a, a++);
+    // 6 6 4
+    // 4 5 5
+    return 0;
+}
